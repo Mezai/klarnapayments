0f9bcb01ba15173111c2800cf58f13ddb56fb00e
Johan Tedenmark <johan.tedenmark@hotmail.se>
2015-11-19 22:09:51 +0100

    added constant to require in PrestaCore

--- classes/KlarnaPrestashopCore.php
+++ classes/KlarnaPrestashopCore.php
@@ -33,17 +33,17 @@ require_once KLARNA_DIRECTORY.'/libs/transport/xmlrpc-3.0.0.beta/lib/xmlrpc_wrap
 
 require_once KLARNA_DIRECTORY.'/libs/Klarna.php';
 
-require_once '/includes/KlarnaPrestaConfig.php';
-require_once '/includes/KlarnaPrestaApi.php';
-require_once '/includes/KlarnaPClassesHandler.php';
-require_once '/includes/KlarnaCheckoutService.php';
-require_once '/includes/KlarnaPrestaEncoding.php';
-require_once '/includes/KlarnaOrderManagement.php';
-require_once '/includes/KlarnaInvoiceFeeHandler.php';
-require_once '/includes/KlarnaGoodsList.php';
-require_once '/includes/KlarnaConfigHandler.php';
-require_once '/includes/KlarnaCountryLogic.php';
-require_once '/includes/KlarnaCalculationHandler.php';
-require_once '/includes/KlarnaLocalization.php';
-require_once '/includes/KlarnaValidation.php';
-require_once '/includes/KlarnaAdressPresta.php';
+require_once KLARNA_DIRECTORY.'/classes/includes/KlarnaPrestaConfig.php';
+require_once KLARNA_DIRECTORY.'/classes/includes/KlarnaPrestaApi.php';
+require_once KLARNA_DIRECTORY.'/classes/includes/KlarnaPClassesHandler.php';
+require_once KLARNA_DIRECTORY.'/classes/includes/KlarnaCheckoutService.php';
+require_once KLARNA_DIRECTORY.'/classes/includes/KlarnaPrestaEncoding.php';
+require_once KLARNA_DIRECTORY.'/classes/includes/KlarnaOrderManagement.php';
+require_once KLARNA_DIRECTORY.'/classes/includes/KlarnaInvoiceFeeHandler.php';
+require_once KLARNA_DIRECTORY.'/classes/includes/KlarnaGoodsList.php';
+require_once KLARNA_DIRECTORY.'/classes/includes/KlarnaConfigHandler.php';
+require_once KLARNA_DIRECTORY.'/classes/includes/KlarnaCountryLogic.php';
+require_once KLARNA_DIRECTORY.'/classes/includes/KlarnaCalculationHandler.php';
+require_once KLARNA_DIRECTORY.'/classes/includes/KlarnaLocalization.php';
+require_once KLARNA_DIRECTORY.'/classes/includes/KlarnaValidation.php';
+require_once KLARNA_DIRECTORY.'/classes/includes/KlarnaAdressPresta.php';

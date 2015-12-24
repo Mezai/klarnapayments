4d4ebf2f87979653bf62fd42d96f11db179510b2
Johan Tedenmark <johan.tedenmark@hotmail.se>
2015-12-24 16:04:25 +0100

    ps validation

--- controllers/front/checkout.php
+++ controllers/front/checkout.php
@@ -57,126 +57,126 @@ class KlarnaPaymentsCheckoutModuleFrontController extends ModuleFrontController
 		try
 		{
 			$country = Tools::strtoupper(Tools::getValue('country'));
-			$sharedSecret = Configuration::get('KLARNA_SECRET_'.$country.'');
-			
+			$shared_secret = Configuration::get('KLARNA_SECRET_'.$country.'');
+
 			if ((String)Configuration::get('KLARNA_ENVIRONMENT') === 'live')
-			{
-				$connector = Klarna_Checkout_Connector::create((String)$sharedSecret, Klarna_Checkout_Connector::BASE_URL);
-			
-			} else {
-				$connector = Klarna_Checkout_Connector::create((String)$sharedSecret, Klarna_Checkout_Connector::BASE_TEST_URL);
-			}
-				
+				$connector = Klarna_Checkout_Connector::create((String)$shared_secret, Klarna_Checkout_Connector::BASE_URL);
+			else
+				$connector = Klarna_Checkout_Connector::create((String)$shared_secret, Klarna_Checkout_Connector::BASE_TEST_URL);
 
 			$checkout_id = $_SESSION['klarna_order_id'];
-			$klarnaorder = new Klarna_Checkout_Order($connector, $checkout_id);  
+			$klarnaorder = new Klarna_Checkout_Order($connector, $checkout_id);
 			$klarnaorder->fetch();
 
-			if ($klarnaorder['status'] === "checkout_incomplete")
-			{
+			if ($klarnaorder['status'] === 'checkout_incomplete')
 				Tools::redirect('order-opc');
-			}
 
-			if ($klarnaorder['status']  === "checkout_complete")
+			if ($klarnaorder['status'] === 'checkout_complete')
 			{
 				$id_cart = $klarnaorder['merchant_reference']['orderid1'];
 				$cart = new Cart((int)$id_cart);
 
+				$amount = (int)$klarnaorder['cart']['total_price_including_tax'];
+				$amount = (float)$amount / 100;
+				$shipping = $klarnaorder['shipping_address'];
+				$billing = $klarnaorder['billing_address'];
+				$reservation_number = $klarnaorder['reservation'];
+				if ($country == 'SE' || $country == 'AT' || $country == 'FI' || $country == 'NO')
+					$extra['transaction_id'] = $reference;
 
-				$amount = (int)($klarnaorder['cart']['total_price_including_tax']);
-		 		$amount = (float)($amount/100);
-		 		$shipping = $klarnaorder['shipping_address'];
-		 		$billing = $klarnaorder['billing_address'];
-		 		$reservation_number = $klarnaorder['reservation'];
-		 		if ($country == 'SE' || $country == 'AT' || $country == 'FI' || $country == 'NO')
-		 		{
-		 			$extra['transaction_id'] = $reference;
-		 		}
-		 		$id_customer = (int)Customer::customerExists($shipping['email'], true, false);
-		 		if ($id_customer > 0)
-		 		{
-		 			$customer = new Customer($id_customer);
+				$id_customer = (int)Customer::customerExists($shipping['email'], true, false);
+				if ($id_customer > 0)
+				{
+					$customer = new Customer($id_customer);
 					if ($country === 'DE')
 					{
 						($billing['title'] === 'Herr') ? $customer->id_gender = 1 : $customer->id_gender = 0;
-					
-					} else {
-						
+
+					}
+					else
+					{
+
 						if ($klarnaorder['customer']['gender'] === 'male')
 						{
 							$customer->id_gender = 1;
-							
-						} else {
+
+						}
+						else
+						{
 							$customer->id_gender = 0;
-						
+
 						}
 					}
-					
+
 					$date_of_birth = $klarnaorder['customer']['date_of_birth'];
-					
+
 					$match_date = preg_match('/^(\d{4})(?:-)(\d{2})(?:-)(\d{2})$/', $date_of_birth, $match);
-					
+
 					if ((Bool)$match_date === true)
 					{
 						$customer->years = $match[1];
 						$customer->months = $match[2];
 						$customer->days = $match[3];
-					
-					} 
-					$customer->update();
-					
 
-		 		} else {
-		 			
-                    $customer = new Customer();
-					$customer->firstname = $shipping['given_name'];
-					$customer->lastname =  $shipping['family_name'];
-					$customer->email = $shipping['email'];
-					$customer->passwd =  Tools::passwdGen(8,'ALPHANUMERIC');
-					$customer->is_guest = 1;
-					$customer->id_default_group = (int)(Configuration::get('PS_GUEST_GROUP', null, $cart->id_shop));
-                    if ($klarnaorder['merchant_requested']['additional_checkbox'] === true)
-                    {
-                        $customer->newsletter = 1;
-                        } else {
-                        $customer->newsletter = 0;
 					}
+					$customer->update();
+
+				}
+				else
+				{
+
+					$customer = new Customer();
+					$customer->firstname = $shipping['given_name'];
+					$customer->lastname = $shipping['family_name'];
+					$customer->email = $shipping['email'];
+					$customer->passwd = Tools::passwdGen(8, 'ALPHANUMERIC');
+					$customer->is_guest = 1;
+					$customer->id_default_group = (int)Configuration::get('PS_GUEST_GROUP', null, $cart->id_shop);
+					if ($klarnaorder['merchant_requested']['additional_checkbox'] === true)
+						$customer->newsletter = 1;
+						else
+						$customer->newsletter = 0;
+
 					$customer->optin = 0;
 					$customer->active = 1;
-					
+
 					if ($country === 'DE')
 					{
 						($billing['title'] === 'Herr') ? $customer->id_gender = 1 : $customer->id_gender = 0;
-					
-					} else {
-						
+
+					}
+					else
+					{
+
 						if ($klarnaorder['customer']['gender'] == 'male')
 						{
 							$customer->id_gender = 1;
-							
-						} else {
-							$customer->id_gender = 0;
-						
+
 						}
-							
+						else
+						{
+							$customer->id_gender = 0;
+
+						}
+
 					}
-					
+
 					$date_of_birth = $klarnaorder['customer']['date_of_birth'];
-					
+
 					$match_date = preg_match('/^(\d{4})(?:-)(\d{2})(?:-)(\d{2})$/', $date_of_birth, $match);
-					
+
 					if ((Bool)$match_date === true)
 					{
 						$customer->years = $match[1];
 						$customer->months = $match[2];
 						$customer->days = $match[3];
-					
-					} 
-					
-					$customer->add();	
-		 		}
 
-		 		$delivery_address_id = 0;
+					}
+
+					$customer->add();
+				}
+
+				$delivery_address_id = 0;
 				$invoice_address_id = 0;
 				$shipping_iso = $country_iso_codes[$shipping['country']];
 				$invocie_iso = $country_iso_codes[$billing['country']];
@@ -184,7 +184,8 @@ class KlarnaPaymentsCheckoutModuleFrontController extends ModuleFrontController
 				$invocie_country_id = Country::getByIso($invocie_iso);
 				if ($country == 'SE' || $country == 'FI' || $country == 'NO' || $country == 'AT')
 				{
-					foreach ($customer->getAddresses($cart->id_lang) as $address) {
+					foreach ($customer->getAddresses($cart->id_lang) as $address)
+					{
 
 						if ($address['firstname'] == $shipping['given_name'] && $address['lastname'] == $shipping['family_name'] && $address['city'] == $shipping['city'] && $address['address2'] == $shipping['care_of']
 							&& $address['address1'] == $shipping['street_address'] && $address['postcode'] == $shipping['postal_code'] && $address['phone_mobile'] == $shipping['phone'] && $address['id_country'] == $shipping_country_id)
@@ -205,8 +206,8 @@ class KlarnaPaymentsCheckoutModuleFrontController extends ModuleFrontController
 
 				if ($country == 'DE')
 				{
-					$street_address_shipping_de = $shipping['street_name'] . $shipping['street_number'];
-					$street_address_billing_de = $billing['street_name'] . $billing['street_number'];
+					$street_address_shipping_de = $shipping['street_name'].$shipping['street_number'];
+					$street_address_billing_de = $billing['street_name'].$billing['street_number'];
 
 					foreach ($customer->getAddresses($cart->id_lang) as $address)
 					{
@@ -223,27 +224,30 @@ class KlarnaPaymentsCheckoutModuleFrontController extends ModuleFrontController
 							$cart->id_address_invoice = $address['id_address'];
 							$invoice_address_id = $address['id_address'];
 						}
-					}	
+					}
 				}
-				
-				if($invoice_address_id==0)
+
+				if ($invoice_address_id == 0)
 				{
 					$address = new Address();
 					$address->firstname = $billing['given_name'];
 					$address->lastname = $billing['family_name'];
 
 					if ($country == 'SE' || $country == 'FI' || $country == 'NO' || $country == 'AT')
-					{ 
+					{
 						if (Tools::strlen($billing['care_of']) > 0)
 						{
 							$address->address1 = $billing['care_of'];
 							$address->address2 = $billing['street_address'];
-						} else {
+						}
+						else
+						{
 							$address->address1 = $billing['street_address'];
 
 						}
 
-					} elseif ($country == 'DE') {
+					} elseif ($country == 'DE')
+					{
 
 						$address->address1 = $street_address_billing_de;
 
@@ -254,29 +258,32 @@ class KlarnaPaymentsCheckoutModuleFrontController extends ModuleFrontController
 					$address->city = $billing['city'];
 					$address->id_country = $invocie_country_id;
 					$address->id_customer = $customer->id;
-					$address->alias = "Klarna Address";
+					$address->alias = 'Klarna Address';
 					$address->add();
 					$cart->id_address_invoice = $address->id;
 					$invoice_address_id = $address->id;
 				}
-				if($delivery_address_id==0)
+				if ($delivery_address_id == 0)
 				{
 					$address = new Address();
 					$address->firstname = $shipping['given_name'];
 					$address->lastname = $shipping['family_name'];
 
 					if ($country == 'SE' || $country == 'FI' || $country == 'NO' || $country == 'AT')
-					{ 
+					{
 						if (Tools::strlen($shipping['care_of']) > 0)
 						{
 							$address->address1 = $shipping['care_of'];
 							$address->address2 = $shipping['street_address'];
-						} else {
+						}
+						else
+						{
 							$address->address1 = $shipping['street_address'];
 
 						}
 
-					} elseif ($country == 'DE') {
+					} elseif ($country == 'DE')
+					{
 
 						$address->address1 = $street_address_shipping_de;
 
@@ -288,13 +295,13 @@ class KlarnaPaymentsCheckoutModuleFrontController extends ModuleFrontController
 					$address->phone_mobile = $shipping['phone'];
 					$address->id_country = $shipping_country_id;
 					$address->id_customer = $customer->id;
-					$address->alias = "Klarna Address";
+					$address->alias = 'Klarna Address';
 					$address->add();
 					$cart->id_address_delivery = $address->id;
 					$delivery_address_id = $address->id;
 				}
 
-				$new_delivery_options[(int)($delivery_address_id)] = $cart->id_carrier.',';
+				$new_delivery_options[(int)$delivery_address_id] = $cart->id_carrier.',';
 				$new_delivery_options_serialized = serialize($new_delivery_options);
 
 					Db::getInstance()->Execute('
@@ -302,7 +309,7 @@ class KlarnaPaymentsCheckoutModuleFrontController extends ModuleFrontController
 						SET `delivery_option` = \''.pSQL($new_delivery_options_serialized).'\'
 						WHERE `id_cart` = '.(int)$cart->id);
 
-					if($cart->id_carrier>0)
+					if ($cart->id_carrier > 0)
 						$cart->delivery_option = $new_delivery_options_serialized;
 					else
 						$cart->delivery_option = '';
@@ -327,40 +334,41 @@ class KlarnaPaymentsCheckoutModuleFrontController extends ModuleFrontController
 					SET `secure_key` = \''.pSQL($customer->secure_key).'\'
 					WHERE `id_cart` = '.(int)$cart->id);
 
-
 				$cache_id = 'objectmodel_cart_'.$cart->id.'_0_0';
 				Cache::clean($cache_id);
 
-		 		$cart = new Cart($cart->id);
-		 		$this->module->validateOrder($cart->id, Configuration::get('KLARNA_OS_CHECKOUT'), $amount, $this->module->displayName, $reservation_number, $extra, NULL,false,$customer->secure_key);
-		 		Db::getInstance()->Execute('
-				INSERT INTO `'._DB_PREFIX_.'klarna_orders` (`id_order`, `id_reservation`, `customer_firstname`, `customer_lastname`, `payment_status`, `customer_country`)
-				VALUES ('.(int)$this->module->currentOrder.', \''.pSQL($reservation_number).'\', \''.pSQL($billing['given_name']).'\', \''.pSQL($billing['family_name']).'\', \''.pSQL($klarnaorder['status']).'\', \''.pSQL(Tools::strtoupper($shipping['country'])).'\')');
+				$cart = new Cart($cart->id);
+				$this->module->validateOrder($cart->id, Configuration::get('KLARNA_OS_CHECKOUT'), $amount, $this->module->displayName,
+				$reservation_number, $extra, null, false, $customer->secure_key);
 
-		 		$reference_ps = Order::getUniqReferenceOf((int)$this->module->currentOrder);
+				Db::getInstance()->Execute('
+				INSERT INTO `'._DB_PREFIX_.'klarna_orders` (`id_order`, `id_reservation`, `customer_firstname`,
+				`customer_lastname`, `payment_status`, `customer_country`)
+				VALUES ('.(int)$this->module->currentOrder.', \''.pSQL($reservation_number).'\', \''.pSQL($billing['given_name']).'\', \''.
+				pSQL($billing['family_name']).'\', \''.pSQL($klarna_order['status']).'\', \''.pSQL(Tools::strtoupper($shipping['country'])).'\')');
 
-		 		$update['status'] = 'created';  
-				$update['merchant_reference'] = array(  
+				$reference_ps = Order::getUniqReferenceOf((int)$this->module->currentOrder);
+
+				$update['status'] = 'created';
+				$update['merchant_reference'] = array(
 					'orderid1' => (String)$cart->id,
 					'orderid2' => (String)$reference_ps
-				);  
-				$klarnaorder->update($update);  
+				);
+				$klarnaorder->update($update);
 
 			}
 
-			$sql = 'SELECT id_order FROM '._DB_PREFIX_.'orders WHERE id_cart='.(int)($klarnaorder['merchant_reference']['orderid1']);
+			$sql = 'SELECT id_order FROM '._DB_PREFIX_.'orders WHERE id_cart='.(int)$klarnaorder['merchant_reference']['orderid1'];
 			$result = Db::getInstance()->getRow($sql);
 
 			$snippet = $klarnaorder['gui']['snippet'];
 			$this->context->smarty->assign(array(
 					'klarna_html' => $snippet,
-					'HOOK_ORDER_CONFIRMATION' => $this->displayOrderConfirmation((int)($result['id_order']))
+					'HOOK_ORDER_CONFIRMATION' => $this->displayOrderConfirmation((int)$result['id_order'])
 
 			));
-			
-			unset($_SESSION['klarna_order_id']);  
 
-
+			unset($_SESSION['klarna_order_id']);
 
 		} catch (Klarna_Checkout_ApiErrorException $e) {
 			Logger::addLog('Klarna module: '.htmlspecialchars($e->getMessage()));
